6cd682fab673c7943dad39d8619f8cd9b20942a0
Moss Green <gufeifei@outlook.com>
2016-07-03 19:45:15 +1200

    update models

--- MvcMusicStore/Models/Artist.cs
+++ MvcMusicStore/Models/Artist.cs
@@ -5,14 +5,9 @@ using System.Web;
 
 namespace MvcMusicStore.Models
 {
-    public class Genre
+    public class Artist
     {
-        public int GenreId { get; set; }
-
+        public int  ArtistId { get; set; }
         public string Name { get; set; }
-
-        public string Description { get; set; }
-        
-        public List<Album> Albums { get; set; }
     }
 }
