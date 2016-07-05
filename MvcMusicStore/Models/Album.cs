c4922bbf991e2441083461d97607f66c9d5f1a0d
Moss Green <gufeifei@outlook.com>
2016-07-05 13:57:53 +1200

    updated HomeController

--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -35,5 +35,8 @@ namespace MvcMusicStore.Models
         public virtual Genre Genre { get; set; }
 
         public virtual Artist Artist { get; set; }
+
+        public virtual List<OrderDetail> OrderDetails { get; set; }
+
     }
 }
