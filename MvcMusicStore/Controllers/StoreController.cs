ca6cf06715095f49f333901d5ec3b7bf7d11a0c8
Moss Green <gufeifei@outlook.com>
2016-07-05 13:38:41 +1200

    create and update  GenreMenu partial view

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -36,5 +36,13 @@ namespace MvcMusicStore.Controllers
             var album = storeDB.Albums.Find(id);
             return View(album);
         }
+
+        // GET: /Store/GenreMenu
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var genres = storeDB.Genres.ToList();
+            return PartialView(genres);
+        }
     }
 }
