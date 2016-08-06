9d5210c3111150ccb80793beab7e68ad8f326084
Moss Green <gufeifei@outlook.com>
2016-08-06 20:34:53 +0800

    error with starting index page. need to fix...

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using MvcMusicStore.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
